801441f51a0940cd9a81beb7eda666dfd01d1adf
landlessness <brian@landlessness.net>
2011-12-16 20:33:51 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taletail::Application.config.secret_token = 'ded800336aa51d1675bbd5d8c48706a538322b07566828c71963397643f3c666dc545624173c20a8debecda68436af4948e20db6faea3adf84cd17d52fd94de6'
